a1a05bb0f9755108691cfdeace9b4e73dc6c3a72
MrLawli3t <sivertgabriel@gmail.com>
2025-08-22 00:31:33 +0200

    Blueprint Updates for 2.16

---
+++ Source/Warrior/Private/WarriorTypes/WarriorStructTypes.cpp
@@ -0,0 +1,11 @@
+// Lawliet Studios All Rights Reserved
+
+
+#include "WarriorTypes/WarriorStructTypes.h"
+
+#include "AbilitySystem/Abilities/WarriorGameplayAbility.h"
+
+bool FWarriorHeroAbilitySet::IsValid() const
+{
+	return InputTag.IsValid() && AbilityToGrant;
+}
---
+++ Source/Warrior/Public/WarriorTypes/WarriorStructTypes.h
@@ -0,0 +1,40 @@
+// Lawliet Studios All Rights Reserved
+
+#pragma once
+#include "GameplayTagContainer.h"
+
+#include "WarriorStructTypes.generated.h"
+
+class UInputMappingContext;
+class UWarriorGameplayAbility;
+class UWarriorHeroLinkedAnimLayer;
+
+USTRUCT(BlueprintType)
+struct FWarriorHeroAbilitySet
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, meta = (Categories = "InputTag"))
+	FGameplayTag InputTag;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	TSubclassOf<UWarriorGameplayAbility> AbilityToGrant;
+
+	bool IsValid() const;
+	
+};
+
+USTRUCT(BlueprintType)
+struct FWarriorHeroWeaponData
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	TSubclassOf<UWarriorHeroLinkedAnimLayer> WeaponAnimLayerClass;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	UInputMappingContext* WeaponInputMappingContext = nullptr;
+	
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, meta = (TitleProperty = "InputTag"))
+	TArray<FWarriorHeroAbilitySet> DefaultWeaponAbilities;
+};
